6fc50f15230fee755f60722b56540c4c2a50fb49
Raniah06 <raniafadil06@gmail.com>
2023-11-12 09:08:02 -0800

    commit message

---
+++ _putchar.c
@@ -0,0 +1,10 @@
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: 1 on success, -1 on error.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
